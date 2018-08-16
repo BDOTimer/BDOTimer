67de14f2c093bf0fa3a1604cc8aa0b5f392478ed
BDOTimer <sneg1917@bk.ru>
2018-08-16 16:11:28 +0300

    проверка местной локали на ".1251'

--- MyTimer-2018ver2/Sources/Main_ver2.cpp
+++ MyTimer-2018ver2/Sources/Main_ver2.cpp
@@ -1,14 +1,10 @@
 ﻿///ooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooo
-/// MyTimer-ver2.0.0.8                                                         |
+/// MyTimer-ver2.0.0.9                                                         |
 /// IDE VS2010.                                                                |
 /// Тext of this sources must have UTF8 codepage.                              |
 ///ooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooo
 #include "Makros.h"
 #include "!myTimer.h"
-
-//#include <fcntl.h>
-//#include <io.h>
-//_setmode(_fileno(stdout), _O_U16TEXT);
 
 typedef BOOL (WINAPI *FN_SETCONSOLEFONT)(HANDLE, DWORD);
 FN_SETCONSOLEFONT SetConsoleFont;
@@ -16,24 +12,54 @@ void setfont_01()
 {   HMODULE hm = GetModuleHandleW(L"KERNEL32.DLL");
     SetConsoleFont = (FN_SETCONSOLEFONT) GetProcAddress(hm, "SetConsoleFont");
     SetConsoleFont(GetStdHandle(STD_OUTPUT_HANDLE), 9);
+    //6 в Windows 7 ставит Lucida 12 
 }
 
+#include <locale> 
+class cIslang
+{
+public:
+    int f;
+    cIslang()
+    {   look();
+        f = GetSystemDefaultLangID();
+        if (f == 1049)
+        {   setlocale(LC_CTYPE, ".1251");
+            cout << "Есть поддержка русского языка!\n";
+            Sleep(1000);
+            return;
+        }
+        cout << "is type os not russian interface\n";
+        f = 0;
+        _getch();
+    }
+    void look()
+    {   char buffer[255];   
+        LCID cid = GetSystemDefaultLCID();
+        GetLocaleInfo(cid, LOCALE_SENGLANGUAGE, buffer,sizeof(buffer));
+        cout << buffer << std::endl;
+
+        if (PRIMARYLANGID(GetUserDefaultLangID()) == LANG_RUSSIAN)
+            cout << "LANG_RUSSIAN = " << LANG_RUSSIAN << "\n";
+    }
+
+}Islang;
+
+void control_blink(BOOL _b = TRUE)
+{   HANDLE handle = GetStdHandle(STD_OUTPUT_HANDLE);
+    CONSOLE_CURSOR_INFO structCursorInfo;
+    GetConsoleCursorInfo(handle, &structCursorInfo);
+    structCursorInfo.bVisible = _b;
+    SetConsoleCursorInfo( handle, &structCursorInfo );
+}
 
 ///ooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooo
 /// MAIN.                                                                      |
 ///ooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooo
 int main()
-{   /*SetConsoleCP(1251);
-    SetConsoleOutputCP(1251);*/
-    setlocale(LC_CTYPE, ".1251");
-    //setlocale(LC_CTYPE, "");
-    setfont_01();
-
-    /*cout << "Оля любит Яшу.\n";
-    _getch   ();*/
+{   setfont_01();
 
     if(chek_mutex()) return 0;
-    //PRN("Привет!\n");
 
     myicon();
 
